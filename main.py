74e6e39fe2dfdeb1470d8357069ed612cc3f9dbf
PhatPhuckDave <kosmodiskclassic0@gmail.com>
2022-01-19 15:38:01 +0100

    Minor cleanup

--- main.py
+++ main.py
@@ -1,23 +1,10 @@
 import postgresql
 
-# db = postgresql.open('pg://user:password@host:port/db')
-from Generators import *
 from Databases import *
 from Table import *
 from Column import *
 
 db = postgresql.open('pq://demo:demo@127.0.0.1:5432/carService')
-# test = db.prepare('SELECT * FROM cars')
-# column_names
-# column_types
-# parameter_types
-
-# mkemp = db.prepare("INSERT INTO users(id) VALUES ($1)")
-#
-# for i in range(1000, 10000):
-# 	mkemp(i)
-#
-# print(mkemp)
 
 db = PostgreSQLDB()
 db.connect(dbName="carService")
