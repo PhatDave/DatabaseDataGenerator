2acd2353aeca70bb910bb75d4fe019eaf8635501
PhatPhuckDave <kosmodiskclassic0@gmail.com>
2022-01-20 11:41:05 +0100

    Add sqlite3 support

--- Databases.py
+++ Databases.py
@@ -6,7 +6,7 @@ class Database:
 	def __init__(self):
 		self.__connection = None
 
-
+# TODO: Add sqlite3
 class PostgreSQLDB(Database):
 	import postgresql
 	global postgresql
@@ -49,6 +49,62 @@ class PostgreSQLDB(Database):
 		return query
 
 	def getPkSet(self, table: Table):
+		test = self.__connection.prepare(
+			f"SELECT {table.getPkColumnName()} FROM {table.name}")
+		arr = test()
+		arr = [i[0] for i in arr]
+		return set(arr)
+
+
+
+class Sqlite3DB(Database):
+	import sqlite3
+	global sqlite3
+
+	def __init__(self, override=False, wipe=False):
+		self.__connection = None
+		self.__cursor = None
+		self.__table = None
+		self.__wipe = wipe
+		self.__override = override
+
+	def connect(self, dbName="demo.db"):
+		self.__connection = sqlite3.connect(dbName)
+		self.__cursor = self.__connection.cursor()
+
+	def setTable(self, table: Table):
+		self.__table = table
+		if self.__wipe:
+			self.wipeTable(table)
+
+	def wipeTable(self, table: Table):
+		self.__cursor.execute(f"DELETE FROM {table.name} *;")
+		self.__cursor.commit()
+
+	def insertRow(self):
+		query = self.__generateQuery()
+		try:
+			self.__cursor.execute(query.query)
+			self.__cursor.commit()
+		# 	TODO: Fix exception
+		except postgresql.exceptions.UniqueError:
+			if self.__override:
+				pkColumn = query.table.getPkColumnName()
+				pk = query.values[query.names.index(pkColumn)]
+				self.__cursor.execute(
+					f"DELETE FROM {query.table.name} WHERE {pkColumn}={pk};")
+				self.__cursor.commit()
+				self.__cursor.execute(query.query)
+				self.__cursor.commit()
+		return
+
+	def __generateQuery(self):
+		query = Query()
+		query.generate(self.__table)
+		return query
+
+	def getPkSet(self, table: Table):
+		# TODO: rework for sqlite
 		test = self.__connection.prepare(
 			f"SELECT {table.getPkColumnName()} FROM {table.name}")
 		arr = test()
--- main.py
+++ main.py
@@ -1,35 +1,24 @@
 import datetime
 
-import postgresql
-
 from Generators import *
 from Databases import *
 from Table import *
 from Column import *
 
 # https://faker.readthedocs.io/en/master/providers.html
-db = PostgreSQLDB(True, True)
-db.connect(dbName="carService")
+db = Sqlite3DB(True, True)
+db.connect(dbName="demo.db")
 
-users = Table("users")
+users = Table("pokemon")
 users.addColumns([
-	Column("id", SerialGenerator(1), True),
-	Column("city", FakeCityGenerator()),
+	Column("poke_id", SerialGenerator(1), True),
+	Column("name", Fake()),
 	Column("country", FakeCountryGenerator()),
 	Column("postal_number", RandomIntegerGenerator(1e4, 1e5)),
 	Column("street", FakeStreetGenerator()),
 	Column("first_name", FakeFirstNameGenerator()),
 	Column("last_name", FakeLastNameGenerator()),
 	Column("street_number", RandomIntegerGenerator(0, 1000)),
-])
-cars = Table("cars")
-cars.addColumns([
-	Column("id", SerialGenerator(1), True),
-	Column("color", SetGenerator({"red", "blue", "yellow", "green", "purple"})),
-	Column("manufacturer_model", RandomIntegerGenerator(0, 6)),
-	Column("production_year", RandomIntegerGenerator(1990, 2020)),
-	Column("registration", FakeLicensePlateGenerator()),
-	Column("userid", SetGenerator(db.getPkSet(users), False)),
 ])
 
 db.wipeTable(cars)
